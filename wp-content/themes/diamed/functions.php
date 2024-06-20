c4d172b0b454b83148cb2901e88ec381a8a0c3ca
Arsesya <earseniy42@gmail.com>
2024-06-20 20:15:45 +0400

    header

---
+++ wp-content/themes/diamed/functions.php
@@ -0,0 +1,12 @@
+<?php
+function my_theme_enqueue_styles() {
+    wp_enqueue_style('main-style', get_stylesheet_uri());
+}
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action('admin_print_scripts', 'print_emoji_detection_script');
+remove_action('admin_print_styles', 'print_emoji_styles');
+
+
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
+?>
---
+++ wp-content/themes/diamed/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<header>
+    <div class="container">
+        <div class="logo">
+            <img src="<?php echo get_template_directory_uri(); ?>/src/img/logo-header.svg" alt="Логотип">
+        </div>
+        <nav class="main-menu">
+            <ul class="menu">
+                <li>Услуги</li>
+                <li>Обучение гирудотерапии</li>
+                <li>О клинике</li>
+                <li>Врачи</li>
+                <li>Статьи</li>
+                <li>Цены</li>
+                <li>Расписание</li>
+                <li>Контакты</li>
+            </ul>
+        </nav>
+        <div class="header-button">
+            <button>Записаться на сеанс</button>
+        </div>
+    </div>
+</header>
+<?php wp_footer(); ?>
+</body>
+</html>
